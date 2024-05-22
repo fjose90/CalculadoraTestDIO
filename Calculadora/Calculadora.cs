b32b82ce021ff85e1166c51d8cefe29a61112c14
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-05-22 11:46:40 -0300

    first commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        public int Somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        public int Subtrair(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+        public int Multiplicar(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+        public List<string> Historico()
+        {
+            return new List<string>();
+        }
+    }
+}
